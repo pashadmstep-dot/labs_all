8c84719d4730091e7e7227e5bbddaed226f3c647
Pavel <pasha.dm.step@gmail.com>
2025-09-10 23:15:49 +0300

    изм7

--- лаба1.cpp
+++ лаба1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 int main() {
+	setlocale(LC_ALL, "RU");
 	double start, end, step;
 	std::cout << "Введите начальное значение x: ";
 	std::cin >> start; // Задаем исходное значение переменной
